5d9ffec8c286888b91c17a8271771265ca604ee6
carljbowman <carljbowman@gmail.com>
2020-09-13 11:34:25 -0400

    Initial push

---
+++ components/footer.js
@@ -0,0 +1,28 @@
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <footer>
+                <div class="site-links">
+                    <h4>Site</h4>
+                    <ol>
+                        <li>
+                            <a class="arrow" href="../index.html">Home</a>
+                        </li>
+                    </ol>
+                </div>
+                <div class="about">
+                    <h4>Feedback (?)</h4>
+                    <p>
+                        See a mushroom that has been misidentified, some information
+                        that doesn't look right, or have general feedback?
+                        Check out the
+                        <a href="https://github.com/carljbowman/mushrooms" target=”_blank>GitHub repository</a>
+                        and file an issue with your feedback.
+                    </p>
+                </div>
+        </footer>
+    `;
+  }
+}
+
+customElements.define('site-footer', Footer);
