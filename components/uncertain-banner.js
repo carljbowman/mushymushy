2495056367765d62ed6f2c964f98a74cf11e48ea
carljbowman <carljbowman@gmail.com>
2021-03-19 16:11:30 -0400

    Update uncertain-banner.js

--- components/uncertain-banner.js
+++ components/uncertain-banner.js
@@ -5,8 +5,8 @@ class UncertainBanner extends HTMLElement {
                 <div class="row">
                     <p>
                         The identification of this mushroom is uncertain.
-                        If you have insights, either if this is correct or
-                        incorrect, check out GitHub to file an issue.
+                        If you have insights into weather it is correct or
+                        not, plase visit GitHub to file an issue.
                     </p>
                     <a href="https://github.com/carljbowman/mushymushy" target=”_blank>
                         <button>Go to GitHub Repro</button>
